b539c2a33f154b5c31eb2319f656a547866e3224
luanvsalmeida <luanalmeida@alunos.utfpr.edu.br>
2025-02-10 20:45:35 -0300

    fix server.js

--- server-side/server.js
+++ server-side/server.js
@@ -7,6 +7,8 @@ const jwt = require("jsonwebtoken");
 const User = require("./model/schema"); // Modelo de usuário
 const autenticar = require("./helper/auth");
 const authRouter = require("./routes/auth");
+const listarUser = require("./routes/listarUser");
+const tagRouter = require("./routes/tags")
 const app = express();
 
 // Configurações
@@ -20,34 +22,14 @@ mongoose
   .connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("🔥 Conectado ao MongoDB"))
   .catch((err) => console.error("Erro ao conectar ao MongoDB:", err));
-
-app.use(router);
-
-// Rota para criar um usuário
-app.post("/users", async (req, res) => {
-  try {
-    const novoUsuario = new User(req.body);
-    await novoUsuario.save();
-    res.status(201).json(novoUsuario);
-  } catch (err) {
-    res.status(400).json({ error: err.message });
-  }
-});
-
-// Rota para listar usuários
-app.get("/users", async (req, res) => {
-  const users = await User.find();
-  res.json(users);
-});
-
-
-
-// Exemplo de rota protegida
-app.get("/perfil", autenticar, async (req, res) => {
-  const usuario = await User.findById(req.usuario.id); // O ID vem do token
-  res.json({ nome: usuario.nome, email: usuario.email });
-});
-
+  
+/**Rotas para cadastro login e listar usuarios */
+//rota para login e cadastro
+app.use("/auth", authRouter);
+//rota para listar os usuários
+app.use("/listarUser",listarUser);
+//rota para as tags
+app.use("/", tagRouter);
 
 
 // Configuração da porta
